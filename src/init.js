d33053fa17459c6a4ecd6c4ef84bb9dff39195cc
rjsgn1209 <huu1209@naver.com>
2021-11-07 21:28:24 +0900

    created CRUD for mongoDB

---
+++ src/init.js
@@ -0,0 +1,11 @@
+import "./db";
+import "./models/Video";
+import app from "./server";
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`Server listenting on http://localhost:${PORT} 🚀`);
+};
+
+app.listen(PORT, handleListening);
---
+++ src/routers/videoRouter.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  deleteVideo,
+  watch,
+  getEdit,
+  postEdit,
+  getUpload,
+  postUpload,
+} from "../controllers/videoController";
+const videoRouter = express.Router();
+
+videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})/delete", deleteVideo);
+
+export default videoRouter;
